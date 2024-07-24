6400e8e4bc4a8d288301bdcace7844e447312abd
maxwneto <maxwneto@gmail.com>
2024-07-24 15:47:12 -0300

    Add implementation of chapt 09 examples from Python crash course book

---
+++ chapt_09_classes/person.py
@@ -0,0 +1,32 @@
+class person:
+    def __init__(self, name, birth_date, gender, color, email):
+        self.name = name
+        self.birth_date = birth_date
+        self.gender = gender
+        self.color = color      
+        self.email = email
+
+    def __str__(self):
+        return (f'Name: {self.name} \nBirthday: {self.birth_date}, \nGender: {self.gender}, '
+                f'\nEthnicity: {self.color}, \nE-mail: {self.email}')
+    
+    def sleep(self):
+        print("Sleeping")
+    
+    def wakeup(self):
+        print("Waking Up")
+    
+    def eat(self):
+        print("Eating")
+    
+    def walk(self):
+        print("Walking")
+    
+    def sit(self):
+        print("Sitting")
+    
+    def study(self):
+        print("Studying")
+    
+    def talk(self):
+        print("Talking")
